4f54c168081cae7dfb82487357ee4a62b432b8c9
komankamisetty <komankamisetty@gmail.com>
2026-03-30 00:59:55 +0000

    Finished lesson 2

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,29 +1,36 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
+let c = 8;
+console.log(" My lucky number is: " + c);
 
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
-
+let d = 7
+console.log("I sleep for about " + d + " hours. From 10-5.")
 
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
+let favorite_fruit = "mango"
+console.log("My favorite fruit is: " + favorite_fruit + "!")
 
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
-
-
+let a = "Bothell"
+let b = "United States of America"
+console.log("I am going to give you some information about my country and city! Here it goes, first city, then country:");
+console.log(a);
+console.log(b);
+console.log(" Thank you so much! And now onwards to some more information about me.");
 
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
-let x = "I have no pet. I will talk about myself. My name is Koman";
-let y = "I will talk about myself. My age is 14."
-let z = "I will talk about my race. I am Indian."
+let f = "My name is Koman.";
+let y = "My age is 14.";
+let z = "I am Indian.";
 
-console.log("TOday, I am suppossed to talk about my pet, but as I have no pet, I shall talk about myself. Here is my name, age, and race: " + x + " " + y + z + " Thank you. That's it for today!")
+console.log("Today, I am suppossed to talk about my pet, but as I have no pet, I shall talk about myself. Here is my name, age, and race: " + f + " " + y + " " + z + " Thank you. That's it for today!");
