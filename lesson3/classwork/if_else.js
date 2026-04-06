90ce942b356adade011925cd8ba627534b6aecd7
komankamisetty <komankamisetty@gmail.com>
2026-04-06 03:21:23 +0000

    finished homework

--- lesson3/classwork/if_else.js
+++ lesson3/classwork/if_else.js
@@ -3,16 +3,13 @@ let age = Number(prompt("How old are you? "));
 if (age >= 18) {
     console.log("You are eligible to vote!");
 } //it would say that if you are older than 18 or 18, you are eligible to vote, but if you typed something that's under 18, then there would be no answer. That includes using anythign other than numbers.
-
 // if/else: choose 1 of 2 paths. ANd also, e equals to the power of. If I type 1e2, then that means 1 * e to the power of 2, or 1 times 100.
-
-let day = Number(prompt("WHat day of the week is this?"));
-
+let day = prompt("WHat day of the week is this?").toLowerCase(); // keep as string, and make it lowercase so it matches
 if (day === "monday") {
     console.log("It's the start of the week!");  
 } else if (day === "friday") { // these run sequencially
     console.log("It's almost the weekend!");
-} else if (day === "saturday" || "sunday") {  //the double vertical slashes (||) are as an or for this
+} else if (day === "saturday" || day === "sunday") {  //the double vertical slashes (||) are as an or for this
     console.log("It's the weekend!");
 } else {
     console.log("It's a weekday.");
--- lesson3/homework/problems.js
+++ lesson3/homework/problems.js
@@ -1,31 +1,62 @@
 // Problem 1
 // Ask the user to enter a number.
 // Print "Even" if the number is divisible by 2, otherwise print "Odd".
-
-
+const prompt = require("prompt-sync")();
+let number = Number(prompt("Enter a number: "));
+if (number % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
 
 // Problem 2
 // Ask the user for the day of the week (all lowercase).
 // Print "Weekend" if the day is "saturday" or "sunday",
 // else print "Weekday".
-
+let random_number = Number
+(prompt("I generated a random number  between 1 to 10. What is your guess on what the number is?"));
+if (random_number === random_number) {
+    console.log("Weekend");
+} else {
+    console.log("Weekday");
+}
 
 
 // Problem 3
 // Generate a random number between 1 and 10 (inclusive).
 // Ask the user to guess the number.
 // Print "Correct!" if the guess matches the random number, else print "Try again!".
-
+let randomNum = Math.floor(Math.random() * 10) + 1; 
+let guess = Number(prompt("Guess a number between 1 and 10: "));
+if (guess === randomNum) {
+    console.log("Correct!");
+} else {
+    console.log("Try again!");
+}
 
 
 // Problem 4
 // Ask the user for a positive integer.
 // If the number is divisible by 2 and greater than 10, print "Big even number".
 // Otherwise print "Number does not meet criteria".
-
+let Int = Number(prompt("Enter a positive integer: "));
+if (Int % 2 === 0 && Int > 10) {
+    console.log("Big even number");
+} else {
+    console.log("Number does not meet criteria");
+}
 
 
 // Problem 5
 // Ask the user for two numbers.
 // Print which number is larger.
 // If the numbers are equal, print "Numbers are equal".
+let number_one = Number(prompt("Enter the first number: "));
+let number_two = Number(prompt("Enter the second number: "));
+if (number_one > number_two) {
+    console.log(number_one);
+} else if (number_two > number_one) {
+    console.log(number_two);
+} else {
+    console.log("Numbers are equal");
+}
